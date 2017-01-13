559b3d3f11eb6669aff3d24d7d1fed90197ba7c6
PseudoscienceGames <quade.81@gmail.com>
2017-01-13 17:42:12 -0600

    Added PathFinding visuals

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Cursor : MonoBehaviour
 {
 	public PawnAction selectedAction;
 	public bool on = false;
 	public Vector2 gridLoc;
+	public Vector2 oldGridLoc;
+	public GameObject markerPrefab;
 
 	public static Cursor Instance;
 	void Awake() { Instance = this; }
@@ -15,14 +18,19 @@ public class Cursor : MonoBehaviour
 		if (on)
 		{
 			RaycastHit hit;
-
+			oldGridLoc = gridLoc;
 			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, LayerMask.GetMask("Ground")))
 			{
 				Vector2 tempGridLoc = Grid.Instance.RoundToGrid(hit.point);
 				if (IslandData.Instance.tiles.ContainsKey(tempGridLoc) && (selectedAction.possibleMoves.ContainsKey(tempGridLoc) || selectedAction.possibleMoves.Count == 0))
 				{
 					gridLoc = tempGridLoc;
-					transform.position = Grid.Instance.GridToWorld(gridLoc, IslandData.Instance.tiles[gridLoc].height); ;
+					transform.position = Grid.Instance.GridToWorld(gridLoc, IslandData.Instance.tiles[gridLoc].height);
+					if (gridLoc != oldGridLoc && selectedAction.GetComponent<AIControl>() == null && selectedAction.actionName == "Move")
+					{
+						Debug.Log(selectedAction.actionName);
+						ShowPath();
+					}
 				}
 			}
 			if (Input.GetMouseButtonDown(0))
@@ -42,10 +50,31 @@ public class Cursor : MonoBehaviour
 
 	void SetAction()
 	{
+		ClearPath();
 		selectedAction.Act(gridLoc);
 		selectedAction = null;
 		transform.GetChild(0).gameObject.SetActive(false);
 		on = false;
+	}
 
+	void ShowPath()
+	{
+		ClearPath();
+		Vector2 next = gridLoc;
+		while (next != selectedAction.GetComponent<Pawn>().gridLoc)
+		{
+			GameObject marker = Instantiate(markerPrefab, Grid.Instance.GridToWorld(next, IslandData.Instance.tiles[next].height), Quaternion.identity) as GameObject;
+			marker.transform.parent = transform;
+			next = selectedAction.possibleMoves[next];
+		}
+	}
+
+	void ClearPath()
+	{
+		for (int i = transform.childCount - 1; i > 0; i--)
+		{
+			if (transform.GetChild(i).name != "Selector")
+				Destroy(transform.GetChild(i).gameObject);
+		}
 	}
 }
